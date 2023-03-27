6f81c8e72406aa884792766c243fc86dd1eaeb3a
Lakshmi Suma Divvela <lakshmi.suma@ibm.com>
2023-03-27 13:05:16 +0530

    d

--- main.tf
+++ main.tf
@@ -1,12 +1,7 @@
 resource "ibm_container_cluster" "testacc_cluster" {
   name            = "test"
   datacenter      = "dal10"
-  machine_type    = "free"
-  hardware        = "shared"
-  public_vlan_id  = "vlan"
-  private_vlan_id = "vlan"
-  subnet_id       = ["1154643"]
-
+ 
   default_pool_size = 1
 
   labels = {
